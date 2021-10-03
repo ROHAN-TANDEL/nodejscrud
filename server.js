ec7bd53a534b3fab162f8ed14923d56f8a725cbf
rohantandel <67rohan@gmail.com>
2021-10-03 19:56:30 +0530

    added env variable for port

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const   app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
+require('dotenv').config();
+process.env.USER_ID // "239482"
+process.env.USER_KEY // "foobar"
+process.env.NODE_ENV // "development"
+
 app.get('/', (req, res) => { res.send('running nodejs application ! '); console.log('server is read'); });
 
-app.listen(3000, () => console.log('Server ready'));
+app.listen(process.env.SERVER_PORT || 3000, () => console.log('Server ready'));
+
+
