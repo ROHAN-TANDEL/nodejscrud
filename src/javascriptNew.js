67d59d25050123e43aa3018a0c82f8679a4298f4
rohantandel <67rohan@gmail.com>
2022-06-28 20:19:39 +0530

    js summary

--- src/javascriptNew.js
+++ src/javascriptNew.js
@@ -34,7 +34,7 @@ JIT-compiled programming language
 and thus It's slow make is faster how  - use JIT - 
 First interpreter and now compiler why
 so what's the diff betwen usual compiler and this JIT compiler why and what is JIT
-
+https://hacks.mozilla.org/2017/02/a-crash-course-in-just-in-time-jit-compilers/
 https://hacks.mozilla.org/2017/02/a-crash-course-in-just-in-time-jit-compilers/
 
 
